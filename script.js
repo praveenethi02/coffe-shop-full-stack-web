3bdee96fdf98ef572ad2971c3be59546a58d6f42
praveenethi02 <praveenethipola@gmail.com>
2026-01-16 22:11:01 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,5 @@
+let search = document.querySelector('.search-box');
+
+document.querySelector('#search-icon').onclick = () =>{  //arrow function
+      search.classList.toggle('active');  //togle use karanne hide wela tiyena ekak view karagnna hari view ekak hide karagnna hari
+}
