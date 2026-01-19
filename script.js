7fa6f121be8af5049957af3401fdfb1802f56434
praveenethi02 <praveenethipola@gmail.com>
2026-01-19 16:23:34 +0530

    The first part of designing a website to be mobile responsive

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ let search = document.querySelector('.search-box');
 document.querySelector('#search-icon').onclick = () =>{  //arrow function
       search.classList.toggle('active');  //togle use karanne hide wela tiyena ekak view karagnna hari view ekak hide karagnna hari
 }
+
+
+let navbar = document.querySelector('.navbar');
+
+document.querySelector('#menu-icon').onclick = ()=>{
+      navbar.classList.toggle('active');
+}
