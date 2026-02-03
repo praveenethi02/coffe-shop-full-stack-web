893030bff6d2bc8e7636169fe321248fbe6f5b4d
praveenethi02 <praveenethipola@gmail.com>
2026-02-03 12:31:55 +0530

    Designing the nav bar and search icon so that when you click on one, one closes and the other opens.

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let search = document.querySelector('.search-box');
 
 document.querySelector('#search-icon').onclick = () =>{  //arrow function
       search.classList.toggle('active');  //togle use karanne hide wela tiyena ekak view karagnna hari view ekak hide karagnna hari
+      navbar.classList.remove('active'); //navbar eka hide karanna
 }
 
 
@@ -9,4 +10,5 @@ let navbar = document.querySelector('.navbar');
 
 document.querySelector('#menu-icon').onclick = ()=>{
       navbar.classList.toggle('active');
+      search.classList.remove('active'); //search eka hide karanna
 }
